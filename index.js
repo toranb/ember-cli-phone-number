3c047d5abb8abf7793db50f9e9f6f67865b4c57a
toran billups <toranb@gmail.com>
2014-12-29 21:18:19 -0600

    0.1.2

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-phone-number'
+  name: 'ember-cli-phone-number',
+  included: function(app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/jquery-mask-library/lib/mask.js');
+  }
 };
