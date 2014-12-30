418b5301d6d802410546d382812089acebcaafcc
toran billups <toranb@gmail.com>
2014-12-29 19:58:46 -0600

    0.1.0

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Ember-cli-phone-number
 
-* `npm install ember-cli-phone-number`
+[![NPM Downlaads](https://img.shields.io/npm/dm/ember-cli-summernote.svg)](https://www.npmjs.org/package/ember-cli-phone-number)
 
-Then add the following to your template
+## Description
+Ember-cli-phone-number is an Ember CLI add-on. This addon will mask an input of type text to look like a phone number
 
+## Installation
+```
+# install via npm
+$ npm install ember-cli-phone-number --save-dev
+# make ember-cli fetch internal dependencies
+$ ember g ember-cli-phone-number
+```
+
+## Basic Usage
+
+### Handlebar Template
+```
 {{phone-number phone_number=model.phone_number}}
+```
--- addon/components/phone-number.js
+++ addon/components/phone-number.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    inputWithDash: function(e) {
-        this.validate('13,48,49,50,51,52,53,54,55,56,57,45', e);
-    },
     validate: function(strCheck, e) {
         var eventInstance = window.event ? event : e;
         var unicode = eventInstance.charCode ? eventInstance.charCode : eventInstance.keyCode;
@@ -14,6 +11,6 @@ export default Ember.Component.extend({
     didInsertElement: function() {
         var self = this;
         this.$('input').mask('999-999-9999');
-        this.$('input').keypress(function (event) { self.inputWithDash(event); });
+        this.$('input').keypress(function (event) { self.validate('13,48,49,50,51,52,53,54,55,56,57,45', event); });
     }
 });
